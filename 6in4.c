9be7a91a059155c488370f99b796231796f63f2a
Not Mike <notmike@notmike.co.uk>
2019-03-27 01:42:39 +0000

    Initial Commit

---
+++ 6in4.c
@@ -0,0 +1,94 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/ioctl.h>
+#include <stdio.h>
+#include <linux/if.h>
+#include <linux/if_tun.h>
+#include <fcntl.h>
+
+#define MAX(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+_a > _b ? _a : _b; })
+
+int main(int argc, char* argv[])
+{
+	if (argc != 4)
+	{
+		printf("Usage: %s [Local IPv4] [Remote IPv4] [Local IPv6]");
+	}
+	printf("LocalIPv4: %s\n", argv[1]);
+	printf("RemoteIPv4: %s\n", argv[2]);
+	printf("LocalIPv6: %s\n", argv[3]);
+
+	int raw_socket = socket(AF_INET, SOCK_RAW, 41);
+	struct sockaddr_in Address;
+	Address.sin_family = AF_INET;
+	inet_pton(AF_INET, &Address.sin_addr, argv[1]);
+	if (bind(raw_socket, (struct sockaddr*)&Address, sizeof(Address)))
+	{
+		printf("Error binding socket.\n");
+		return 0;
+	}
+	int tun_device;
+	if ((tun_device = open("/dev/net/tun", O_RDWR)) < 0)
+	{
+		perror("tuntap not found");
+		return -1;
+	}
+	struct ifreq ifr = { 0 };
+	ifr.ifr_flags = IFF_TUN;
+	if ((ioctl(tun_device, TUNSETIFF, (void *)&ifr)) < 0)
+	{
+		close(tun_device);
+		perror("unable to init tuntap");
+		return -1;
+	}
+	fcntl(tun_device, F_SETFL, O_NONBLOCK);
+	int status;
+	if (fork() == 0) execlp("ip", "ip", "link", "set", ifr.ifr_name, "up", NULL); else wait(&status);
+	if (fork() == 0) execlp("ip", "ip", "link", "set", ifr.ifr_name, "mtu", "1280", NULL); else wait(&status);
+	if (fork() == 0) execlp("ip", "ip", "addr", "add", argv[3], "dev", ifr.ifr_name, NULL); else wait(&status);
+	if (fork() == 0) execlp("ip", "ip", "-6", "route", "delete", "default", NULL); else wait(&status);
+	if (fork() == 0) execlp("ip", "ip", "-6", "route", "replace", "default", "dev", ifr.ifr_name, "prio", "1", "hoplimit", "64", NULL); else wait(&status);
+
+	unsigned char Buffer[2048];
+	int AddressLen;
+	while (1)
+	{
+		fd_set read_fds;
+		FD_ZERO(&read_fds);
+		FD_SET(raw_socket, &read_fds);
+		FD_SET(tun_device, &read_fds);
+		int FD_MAX = MAX(tun_device, raw_socket);
+		select(FD_MAX + 1, &read_fds, NULL, NULL, NULL);
+		if (FD_ISSET(raw_socket, &read_fds))
+		{
+			memset(&Address, 0, sizeof(Address));
+			Address.sin_family = AF_INET;
+			int ret = recvfrom(raw_socket, (char*)Buffer, sizeof(Buffer) - 4, 0, (struct sockaddr*)&Address, &AddressLen);
+			if (ret > 0)
+			{
+				char IP[128];
+				inet_ntop(AF_INET, &Address.sin_addr, IP, sizeof(IP));
+				Buffer[16] = 0;
+				Buffer[17] = 0;
+				Buffer[18] = 0x86;
+				Buffer[19] = 0xdd;
+				write(tun_device, Buffer + 16, ret, 0);
+			}
+		}
+		if (FD_ISSET(tun_device, &read_fds))
+		{
+			int ret = read(tun_device, Buffer, 2048, 0);
+			if (ret > 0)
+			{
+				memset(&Address, 0, sizeof(Address));
+				Address.sin_family = AF_INET;
+				inet_pton(AF_INET, argv[2], &Address.sin_addr);
+				sendto(raw_socket, Buffer + 4, ret - 4, 0, (struct sockaddr*)&Address, sizeof(Address));
+			}
+		}
+	}
+	return 0;
+}
